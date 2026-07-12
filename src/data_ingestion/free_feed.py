61fe101f28407d8b191f527599ce991b574a9f58
sprzones <sprzones@gmail.com>
2026-07-12 07:38:19 +0530

    Add FreeDataFeed class to fetch macro telemetry

---
+++ src/data_ingestion/free_feed.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+
+class FreeDataFeed:
+    def fetch_macro_telemetry(self):
+        return {
+            "USDINR": "83.5",
+            "CrudeOil": "82.0",
+            "DXY": "104.1",
+            "BTC": "64000"
+        }
